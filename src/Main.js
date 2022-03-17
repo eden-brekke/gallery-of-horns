108e588a79eb2ea0db83ca4d4f3148ad3e813d55
Eden Brekke <eden.lorrai@gmail.com>
2022-03-17 10:12:07 -0700

    some css changes

--- src/Main.js
+++ src/Main.js
@@ -40,7 +40,7 @@ class Main extends React.Component {
         />
 
         <Container>
-          <Row>
+          <Row xs={1} sm={2} md={3} lg={3}>
             {this.state.data.map((beast, index) => (
               <Col key={index}>
                 <HornedBeast
